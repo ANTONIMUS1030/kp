fb0f45d0bd0b60af0043c8086be8bceff4f86d8a
ANTONIMUS1030 <78993908+ANTONIMUS1030@users.noreply.github.com>
2021-03-21 17:18:11 -0400

    Update outros.js

--- src/outros.js
+++ src/outros.js
@@ -1,41 +1,42 @@
 const iklan = () => { 
-	return `           
-╔══✪〘 *OUTROS* 〙✪══
+	return `  
+╔═══════════════════         
+║ 	    • ──── ✾ ──── •
+║           » OUTROS «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}linkgp
+║ ➸ ${prefix}simih [1/0]
+║ ➸ ${prefix}marcar*
+║ ➸ ${prefix}add [@]*
+║ ➸ ${prefix}banir [@]*
+║ ➸ ${prefix}promover [@]*
+║ ➸ ${prefix}rebaixar*
+║ ➸ ${prefix}admins*
+║ ➸ ${prefix}marcar2*
+║ ➸ ${prefix}bc [texto]* (ele faz uma ™)
+║ ➸ ${prefix}marcar3*
+║ ➸ ${prefix}bloqueados*
+║ ➸ ${prefix}bloquear [@]*
+║ ➸ ${prefix}desbloquear [@]*
+║ ➸ ${prefix}limpar*
+║ ➸ ${prefix}bc [ *texto* ]*
+║ ➸ ${prefix}bemvindo [1/0]*
+║ ➸ ${prefix}clonar [@]*
+║ ➸ ${prefix}help1*
+║ ➸ ${prefix}dono*
+║ ➸ ${prefix}owner*
+║ ➸ ${prefix}tts [texto]*
+║ ➸ ${prefix}setnome*
+║ ➸ ${prefix}termux*
+║ ➸ ${prefix}setfoto*
+║ ➸ ${prefix}grupoinfo*
+║ ➸ ${prefix}ytmp4*
+║ ➸ ${prefix}bomdia*
+║ ➸ ${prefix}boanoite*
+║ ➸ ${prefix}marcar*
+║ ➸ ${prefix}marcar2*
+║ ➸ ${prefix}marcar3* 
 ║
-╠➥ *${prefix}linkgp*
-╠➥ *${prefix}simih [1/0]*
-╠➥ *${prefix}marcar*
-╠➥ *${prefix}add [@]*
-╠➥ *${prefix}banir [@]*
-╠➥ *${prefix}promover [@]*
-╠➥ *${prefix}rebaixar*
-╠➥ *${prefix}admins*
-╠➥ *${prefix}marcar2*
-╠➥ *${prefix}bc [texto]* (ele faz uma ™)
-╠➥ *${prefix}marcar3*
-╠➥ *${prefix}bloqueados*
-╠➥ *${prefix}bloquear [@]*
-╠➥ *${prefix}desbloquear [@]*
-╠➥ *${prefix}limpar*
-╠➥ *${prefix}bc [ *texto* ]*
-╠➥ *${prefix}bemvindo [1/0]*
-╠➥ *${prefix}clonar [@]*
-╠➥ *${prefix}help1*
-╠➥ *${prefix}dono*
-╠➥ *${prefix}owner*
-╠➥ *${prefix}tts [texto]*
-╠➥ *${prefix}setnome*
-╠➥ *${prefix}termux*
-╠➥ *${prefix}setfoto*
-╠➥ *${prefix}grupoinfo*
-╠➥ *${prefix}ytmp4*
-╠➥ *${prefix}bomdia*
-╠➥ *${prefix}boanoite*
-╠➸ *${prefix}marcar*
-╠➸ *${prefix}marcar2*
-╠➸ *${prefix}marcar3*
-║
-╚═〘  *DARKBOT* 〙
-`
+╚═〘 𝕊𝔼ℝ𝔾𝕀ℕ 𝔻𝕆 𝔾ℝ𝔸𝕍𝔼 𝐁𝐎𝐓 〙`
 }
 exports.iklan = iklan
